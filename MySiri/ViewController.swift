a6b4e88bfcf4db16a90e7615272ac9b5db0b07ca
Ryo <ryoouchi0315@gmail.com>
2020-09-05 19:31:43 +0900

    Initial Commit

---
+++ MySiri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySiri
+//
+//  Created by Ryo on 2020/09/05.
+//  Copyright © 2020 Ryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
